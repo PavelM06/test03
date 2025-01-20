20bdda5d87e06ed8b9f8e7ab556676337b159ef2
Pavel <kademok123@gmail.com>
2025-01-20 13:34:07 +0400

    constructor with parametrs, object car2

--- test03_1.cpp
+++ test03_1.cpp
@@ -3,6 +3,7 @@
 class Car {
 	public:
 	Car();
+	Car(int newP, double newV);
 	double 	X;
 	int 	P;
 	double 	V;
@@ -13,11 +14,17 @@ Car::Car() {
 	X = 0;
 }
 
+Car::Car(int newP,double newV){
+	P = newP;
+	V = newV;
+}
+
 main () {
 	using namespace std;
 	cout << "\n Work by Pavel Makarov.\n";
 	
 	Car car;
+	Car car2(3, 65);
 	
 	cout << "\n Car X: " << car.X;
 	cout << "\n Line of car: " << car.P;
@@ -27,9 +34,22 @@ main () {
 	car.P = 4;
 	car.V = 70;
 	
+	
 	cout << "\n \n Car X: " << car.X;
 	cout << "\n Line of car: " << car.P;
 	cout << "\n Speed of car: " << car.V;
+	
+	cout << "\n Car2 X: " << car2.X;
+	cout << "\n Line of car2: " << car2.P;
+	cout << "\n Speed of car2: " << car2.V;
+	
+	car2.X = 200;
+	car2.P = 5;
+	car2.V = 29;
+	
+	cout << "\n \n Car2 X: " << car2.X;
+	cout << "\n Line of car2: " << car2.P;
+	cout << "\n Speed of car2: " << car2.V;
 	
 	return 0;
 }
