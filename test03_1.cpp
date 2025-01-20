34b84358be6ddac5c799452aadf98753ba1467e7
Pavel <kademok123@gmail.com>
2025-01-20 13:21:28 +0400

    initializing fields of object car and output value

--- test03_1.cpp
+++ test03_1.cpp
@@ -9,11 +9,27 @@ class Car {
 	
 };
 
-Car::Car() {}
+Car::Car() {
+	X = 0;
+}
 
 main () {
 	using namespace std;
 	cout << "\n Work by Pavel Makarov.\n";
+	
 	Car car;
+	
+	cout << "\n Car X: " << car.X;
+	cout << "\n Line of car: " << car.P;
+	cout << "\n Speed of car: " << car.V;
+	
+	car.X = 150;
+	car.P = 4;
+	car.V = 70;
+	
+	cout << "\n \n Car X: " << car.X;
+	cout << "\n Line of car: " << car.P;
+	cout << "\n Speed of car: " << car.V;
+	
 	return 0;
 }
